d8998ee4984db0315102164e5ecc12c51106ecd4
Sergey Shmatovskiy <elenergetica87@mail.ru>
2018-10-11 16:33:17 +0300

    try again 3

---
+++ database/factories/UsersFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+use Faker\Generator as Faker;
+
+use App\User;
+use Illuminate\Support\Str;
+
+/*
+php artisan db:seed --class=UsersTableSeeder
+
+ composer dump-autoload -o
+*/
+
+
+$factory->define(User::class, function (Faker $faker) {
+	$active = $faker->boolean;
+	$name = $faker->name;
+
+    return [
+        'name' => $name,
+        'email' => $faker->unique()->safeEmail,
+        'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
+        'image' => '/admin/img/user2-160x160.jpg',
+        'slug' => Str::slug($name),
+        'likesNumber' => 0,
+        'commentsNumber' => 0,
+        'remember_token' => str_random(10),
+        'verify_token' => $active ? null : Str::uuid() ,
+        'status' => $active ? User::STATUS_ACTIVE : User::STATUS_WAIT,
+        'role' => $active ? $faker->randomElement([User::ROLE_USER, User::ROLE_ADMIN]) : User::ROLE_USER,
+    ];
+});
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -8,11 +8,8 @@ class DatabaseSeeder extends Seeder
 
     public function run()
     {
-<<<<<<< HEAD
         // $this->call(UsersTableSeeder::class);
         // $this->call(CategoriesTableSeeder::class);
-=======
         factory(User::class, 10)->create();
->>>>>>> 18c44751e1b11181bc2343ec50628b6fbe43e10d
     }
 }
